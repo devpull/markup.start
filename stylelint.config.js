54434dca421c0e1f9830e9773cf24f3d8647d6eb
devpull <devpull@yandex.ru>
2025-10-01 11:27:01 +0300

    Stylelint config update with bem rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,8 +4,20 @@ export default {
 		'stylelint-config-standard-scss',
 		'stylelint-prettier/recommended',
 	],
-	plugins: ['stylelint-order', 'stylelint-scss'],
+	plugins: [
+		'stylelint-order',
+		'stylelint-scss',
+		'stylelint-selector-bem-pattern',
+	],
 	rules: {
 		// 'scss/dollar-variable-pattern': '^[a-z][a-zA-Z0-9]+$',
+    'plugin/selector-bem-pattern': {
+			preset: 'bem',
+			// componentName: '[a-z-]+',
+			// componentSelectors: {
+			// 	initial: '^\\.{componentName}(?:__[a-z0-9-]+)?(?:--[a-z0-9-]+)?$',
+			// 	combined: '^\\.{componentName}(?:__[a-z0-9-]+)?(?:--[a-z0-9-]+)?$',
+			// },
+		},
 	},
 };
